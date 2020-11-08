ba608bd7d8ca7130ed936511c35fedd21ccc29d5
poemDead <69440589+poemDead@users.noreply.github.com>
2020-11-08 18:13:09 +0900

    石墨登陆

--- week02/shimo/shimo-selenium.py
+++ week02/shimo/shimo-selenium.py
@@ -4,17 +4,14 @@ import time
 try:
     brower = webdriver.Chrome()
 
-    brower.get('https://shimo.im/')
+    brower.get('https://shimo.im/login?from=home')
     time.sleep(2)
 
-    login_button = brower.find_element_by_xpath("//div[@class='entries']/a[2]/button")
-    login_button.click()
-
-    brower.find_element_by_xpath('//*[@id="root"]/div/div[2]/div/div/div/div[2]/div/div/div[1]/div[1]/div/input').send_keys('mofemo4494@cyberper.net')
-    brower.find_element_by_xpath('//*[@id="root"]/div/div[2]/div/div/div/div[2]/div/div/div[1]/div[2]/div/input').send_keys('test123geek')
+    brower.find_element_by_name('mobileOrEmail').send_keys('mofemo4494@cyberper.net')
+    brower.find_element_by_name('password').send_keys('test123geek')
     time.sleep(1)
     brower.find_element_by_xpath("//button[@class='sm-button submit sc-1n784rm-0 bcuuIb']").click()
-    time.sleep(60)
+    time.sleep(10)
 
     cookies = brower.get_cookies()
     title = brower.title
